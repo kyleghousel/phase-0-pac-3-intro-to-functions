602dea99416ded4f4b293222e0c94d9003d659fc
Kyle <kylegranthousel@gmail.com>
2024-03-01 14:25:27 -0500

    pushing modified files

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Follow along with the examples here
+function doNothing() {}
+
+names = ["Jenny", "Steve", "Caitie", "Tim"]
+
+function sayHello(arr) {
+  i = 0;
+  while (i < arr.length) {
+    console.log(`Hello, ${arr[i]}!`);
+    i++
+  }
+}
+sayHello(names);
+
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
+
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+sayHelloToLiz();
+
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+sayHelloToSamip();
+
+function add(x, y) {
+  return x + y;
+}
+
+const sum = add(50,25);
+const summedValue = `The sum of your numbers is: ${sum}.`;
+console.log(summedValue);
+
+
+function sayWhat() {
+  console.log("What.");
+  return "WHAT";
+}
+sayWhat();
